b9e69ba56429e5e10a497abad20301ad686df3d9
LuisKlopp <fbgus333@gmail.com>
2022-08-02 13:25:23 +0900

    ryu test

---
+++ src/redux/modules/toolkit.js
@@ -0,0 +1,19 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+
+
+let todos = createSlice ({
+  name : 'todos',
+  initialState : {
+    todo_1: [
+    ]
+  },
+  reducers: {
+
+  }
+
+
+})
+
+
+export default todos
